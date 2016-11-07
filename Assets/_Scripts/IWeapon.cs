295410a89086554ae1e388ab212dd082570d14f0
Sean Mann <naplandgames@gmail.com>
2016-11-07 14:29:01 -0500

    started working on classes

---
+++ Assets/_Scripts/IWeapon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NG.Procedural.WeaponGenerator
+{
+	public abstract class Weapon
+	{
+		public abstract Quality HandleQuality { get; protected set; }
+		public abstract Quality BladeQuality { get; protected set; }
+
+		public abstract
+
+	}
+
+	public enum Quality
+	{
+		None, Wood, Bronze, Iron, Silver
+	}
+
+	public interface IAttackEffect
+	{
+
+	}
+
+	public struct Attack
+	{
+		public int Damage { get; private set; }
+		public IAttackEffect AttackEffect { get; private set; }
+
+		public Attack(int damage, IAttackEffect attackEffect)
+		{
+			this.Damage = damage;
+			this.AttackEffect = attackEffect;
+		}
+	}
+	
+}
